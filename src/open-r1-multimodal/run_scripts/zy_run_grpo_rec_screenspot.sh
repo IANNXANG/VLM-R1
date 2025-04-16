2b5d357e6f404bb5a83e263eba7bec85db996054
IANNXANG <yx17180324@gmail.com>
2025-04-16 22:47:47 +0800

    更新脚本以优化训练参数，调整保存步数和学习率，增加LORA相关设置，修改运行名称和日志路径，确保与新模型和数据集配置一致

--- src/open-r1-multimodal/run_scripts/zy_run_grpo_rec_screenspot.sh
+++ src/open-r1-multimodal/run_scripts/zy_run_grpo_rec_screenspot.sh
@@ -30,7 +30,45 @@ torchrun --nproc_per_node="4" \
     --attn_implementation flash_attention_2 \
     --num_train_epochs 2 \
     --run_name $RUN_NAME \
-    --save_steps 50 \
+    --save_steps 10 \
+    --save_only_model true \
+    --learning_rate 1e-5 \
+    --use_peft true \
+    --lora_r 64 \
+    --lora_alpha 128 \
+    --lora_dropout 0.05 \
+    --lora_task_type CAUSAL_LM \
+    --freeze_vision_modules true 
+
+
+RUN_NAME="Qwen2.5-VL-7B-GRPO-ScreenSpot-Desktop_tp"
+export LOG_PATH="./debug_log_$RUN_NAME.txt"
+
+torchrun --nproc_per_node="4" \
+    --nnodes="1" \
+    --node_rank="0" \
+    --master_addr="127.0.0.1" \
+    --master_port="12346" \
+    src/open_r1/grpo_rec.py \
+    --deepspeed local_scripts/zero2.json \
+    --output_dir output/$RUN_NAME \
+    --model_name_or_path /c22940/zy/model/Qwen2.5-VL-7B-Instruct \
+    --dataset_name data_config/rec_with_screenspot.yaml \
+    --image_root /c22940/zy/code/VLM-R1/otherdata/ScreenSpot-v2 \
+    --max_prompt_length 1024 \
+    --num_generations 4 \
+    --per_device_train_batch_size 1 \
+    --gradient_accumulation_steps 2 \
+    --logging_steps 1 \
+    --bf16 \
+    --torch_dtype bfloat16 \
+    --data_seed 42 \
+    --report_to wandb \
+    --gradient_checkpointing true \
+    --attn_implementation flash_attention_2 \
+    --num_train_epochs 20000 \
+    --run_name $RUN_NAME \
+    --save_steps 1000 \
     --save_only_model true \
     --learning_rate 1e-5 \
     --use_peft true \
